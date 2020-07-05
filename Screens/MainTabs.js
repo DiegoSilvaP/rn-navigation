583b9ef7c8ef0cdc8dda393ed7917d7e6d39017b
disilvap <diego.silva.pr@outlook.com>
2020-07-04 21:27:23 -0500

    MaterialTopTabNavigator

--- Screens/MainTabs.js
+++ Screens/MainTabs.js
@@ -1,49 +1,36 @@
 import React from 'react';
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import { Feed } from "./Feed";
 import { Home } from "./Home";
 import { About } from "./About";
 
-const Tab = createMaterialBottomTabNavigator();
+const Tab = createMaterialTopTabNavigator();
 
 export function MyTabs() {
   return (
     <Tab.Navigator
       initialRouteName="Home"
-      activeColor="#fff"
-      labelStyle={{ fontSize: 12 }}
-      style={{ backgroundColor: 'tomato' }}
+      tabBarOptions={{
+        activeTintColor: '#fff',
+        indicatorStyle: {backgroundColor: '#f69e7b'},
+        labelStyle: { fontSize: 12 },
+        style: { backgroundColor: '#383e56' },
+      }}
     >
       <Tab.Screen
         name="Home"
         component={Home}
-        options={{
-          tabBarLabel: 'Home',
-          tabBarIcon: ({ color }) => (
-            <Icon name="home" color={color} size={26} />
-          ),
-        }}
+        options={{ tabBarLabel: 'Home' }}
       />
       <Tab.Screen
         name="About"
         component={About}
-        options={{
-          tabBarLabel: 'About',
-          tabBarIcon: ({ color }) => (
-            <Icon name="bell" color={color} size={26} />
-          ),
-        }}
+        options={{ tabBarLabel: 'About' }}
       />
       <Tab.Screen
         name="Feed"
         component={Feed}
-        options={{
-          tabBarLabel: 'Feed',
-          tabBarIcon: ({ color }) => (
-            <Icon name="user" color={color} size={26} />
-          ),
-        }}
+        options={{ tabBarLabel: 'Feed' }}
       />
     </Tab.Navigator>
   );
